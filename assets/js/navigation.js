c53b1f143a59f5f18614d426ceb35864a93de769
santiago <caballerosuarezsantiago@gmail.com>
2022-08-13 15:31:43 -0500

    new file, assets(js. img, css)

---
+++ assets/js/navigation.js
@@ -0,0 +1,15 @@
+const navMenu = document.getElementById('container-main_section_links');
+const navToggle = document.getElementById('bar-toggle');
+
+// open or close the menu
+navToggle.addEventListener('click', function() {
+    navMenu.classList.toggle('container-main_section_links_nav-visible');
+
+    // change atrr by arial-label for accessibility
+
+    if (navMenu.classList.contains('container-main_section_links_nav-visible')) {
+        navToggle.setAttribute('aria-label', 'Close navigation');
+    } else {
+        navToggle.setAttribute('aria-label', 'Open navigation');
+    }
+})
